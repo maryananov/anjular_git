79474c9bb2c0b8d8b2e6c3d694b92a00a97b02ff
Marianna <domino.raskol@gmail.com>
2022-11-24 21:46:29 +0200

    first commit

---
+++ constants.js
@@ -0,0 +1,79 @@
+const ShoppingList = [
+  {
+    "id": 1,
+    "title": "Laptop Asus VivoBook 15",
+    "img": "https://www.flanco.ro/media/catalog/product/cache/3869290ebce25e2f88fbb249726321d7/a/s/asus_vivobook_x1500ea-bq2298_fata.jpg",
+    "price": 20000,
+    "description": "X1500EA, 15.6\", Full HD, Intel Core i3-1115G4, 8GB RAM, 256GB SSD"
+  },
+  {
+    "id": 2,
+    "title": "Lenovo IdeaPad 1 15ADA7",
+    "price": 12000,
+    "img": "https://www.flanco.ro/media/catalog/product/cache/3869290ebce25e2f88fbb249726321d7/l/e/lenovo_ip1_82r1006frm_fata.jpg",
+    "description": "15.6\", Full HD, AMD Ryzen 3 3250U, 8GB RAM, 256GB SSD, AMD "
+  },
+  {
+    "id": 3,
+    "title": "Laptop HP 15s-fq5040nq",
+    "price": 15000,
+    "img": "https://www.flanco.ro/media/catalog/product/cache/3869290ebce25e2f88fbb249726321d7/1/5/155521_laptop_hp_main12.jpg",
+    "description": "5.6\", Full HD, Intel Core i3-1215U, 8GB RAM, 256GB SSD, Intel UHD"
+  },
+  {
+    "id": 4,
+    "title": "Laptop Huawei MateBook D15",
+    "price": 14000,
+    "img": "https://www.flanco.ro/media/catalog/product/cache/3869290ebce25e2f88fbb249726321d7/l/a/laptop_huawei_matebookd15_53013ktv_fata_1.jpg",
+    "description": "15.6\", Full HD, Intel Core i5-1135G7, 8GB RAM, 512GB"
+  },
+  {
+    "id": 5,
+    "title": "Laptop Asus X515EA",
+    "price": 17000,
+    "img": "https://www.flanco.ro/media/catalog/product/cache/3869290ebce25e2f88fbb249726321d7/x/5/x515ea-bq850_1.jpg",
+    "description": "Intel Core i3-1115G4, 15.6inch, HD, 8GB, 256GB SSD, Intel Iris XE Graphics, Free"
+  },
+  {
+    "id": 6,
+    "title": "Laptop Huawei MateBook D14",
+    "price": 21000,
+    "img": "https://www.flanco.ro/media/catalog/product/cache/3869290ebce25e2f88fbb249726321d7/l/a/laptop_huawei_53013awa_fata.jpg",
+    "description": "14\", Full HD, Intel Core i3-1115G4, 8GB RAM, 256GB SSD, Intel UHD"
+  },
+  {
+    "id": 7,
+    "title": "Laptop Apple MacBook Air",
+    "price": 40000,
+    "img": "https://www.flanco.ro/media/catalog/product/cache/3869290ebce25e2f88fbb249726321d7/1/4/143995-1_1.jpg",
+    "description": "13\" Retina, Apple M1, 8GB, SSD 256GB, Apple M1 GPU, macOS Big Sur, INT "
+  },
+  {
+    "id": 8,
+    "title": "Laptop Apple MacBook Pro ",
+    "price": 80000,
+    "img": "https://www.flanco.ro/media/catalog/product/cache/3869290ebce25e2f88fbb249726321d7/l/a/laptop_16_apple_silver_5.jpg",
+    "description": "16\" Retina, Apple M1 Pro, 16GB, SSD 512GB, Apple M1 GPU 16 Core"
+  },
+  {
+    "id": 9,
+    "title": "Laptop Apple MacBook Pro ",
+    "price": 75000,
+    "img": "https://www.flanco.ro/media/catalog/product/cache/3869290ebce25e2f88fbb249726321d7/l/a/laptop_apple_silver_5_1.jpg",
+    "description": "14.2\" Retina, Apple M1 Pro, 16GB, SSD 1TB, Apple M1 GPU 16 Core"
+  },
+  {
+    "id": 10,
+    "title": "Laptop HP 255 G8 2E9J5EA",
+    "price": 10000,
+    "img": "https://www.flanco.ro/media/catalog/product/cache/3869290ebce25e2f88fbb249726321d7/h/p/hp_255_g8_2e9j5ea_1.jpg",
+    "description": "AMD Ryzen 3 3250U, 15.6inch, RAM 8GB, SSD 256GB, AMD Radeon"
+  }
+]
+
+const users = [
+  {
+    email: "admin@admin.com",
+    password: "administrator"
+  }
+]
